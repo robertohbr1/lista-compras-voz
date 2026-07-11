ef2d2c03fbd9010fa5486565b8a4dee06e832fa4
Roberto Brum <82846015+robertohbr1@users.noreply.github.com>
2026-07-11 18:13:07 -0300

    feat: setup capacitor and github actions android build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.listacompra.voz',
+  appName: 'Lista de Compras',
+  webDir: 'dist'
+};
+
+export default config;
